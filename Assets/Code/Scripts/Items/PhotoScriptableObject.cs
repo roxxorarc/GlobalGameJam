f3aaed5e18099602a8f1c84dc2b6b3a6b4cdbb8c
roxxorarc <roxxorarc@gmail.com>
2023-01-28 17:21:15 +0100

    added photo scriptable object

---
+++ Assets/Code/Scripts/Items/PhotoScriptableObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Photo", menuName = "Photo")]
+public class PhotoScriptableObject : ScriptableObject
+{
+    public string photoName;
+
+    public Sprite photoSprite;
+
+
+}
